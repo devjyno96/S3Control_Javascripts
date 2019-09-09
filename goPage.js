e6c077364574828f91400e162e78cbd296aec6ed
Jyno <jinho9613@naver.com>
2019-09-09 20:05:09 +0900

    페이지 이동 라이브러리 제작

---
+++ goPage.js
@@ -0,0 +1,10 @@
+var indexPage = '1. index.html'
+var createBoardPage = '2. createBoard.html'
+var listBoardPage = '3. listBoard.html'
+var viewBoardPage = '4. viewBoard.html'
+var uploadObjectPage =  '5. uploadObject.html'
+var viewObjectPage = '6. viewObject.html'
+
+    function goPage(page){
+      location.href= page;
+    }
